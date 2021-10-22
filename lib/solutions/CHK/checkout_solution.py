dd67956e9b141513a7d344960a223f08035affec
sbx_user1051 <sbx_user1051@169.254.158.117>
2021-10-22 13:48:06 +0000

    Fri Oct 22 13:48:06 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,24 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    n_a = 0
+    n_b = 0
+    n_c = 0
+    n_d = 0
+    for sku in skus.split(" "):
+        if(sku == "A"):
+            n_a += 1
+        elif(sku == "B"):
+            n_b += 1
+        elif(sku == "C"):
+            n_c += 1
+        elif(sku == "D"):
+            n_d += 1
+    ret = 0
+    ret += 130 * floor(n_a / 3)
+    ret += 50 * (n_a % 3)
+    ret += 45 * floor(n_b / 2)
+    ret += 30 * (n_b % 2)
+    ret += 20 * n_c
+    ret += 15 * n_d
+
