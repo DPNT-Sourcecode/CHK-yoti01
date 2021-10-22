70b916a7415b3a1c588f613704fa35b4abcf4416
sbx_user1051 <sbx_user1051@169.254.158.117>
2021-10-22 14:13:54 +0000

    Fri Oct 22 14:13:54 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,31 @@
-
+"""+------+-------+------------------------+
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
+| F    | 10    | 2F get one F free      |
+| G    | 20    |                        |
+| H    | 10    | 5H for 45, 10H for 80  |
+| I    | 35    |                        |
+| J    | 60    |                        |
+| K    | 80    | 2K for 150             |
+| L    | 90    |                        |
+| M    | 15    |                        |
+| N    | 40    | 3N get one M free      |
+| O    | 10    |                        |
+| P    | 50    | 5P for 200             |
+| Q    | 30    | 3Q for 80              |
+| R    | 50    | 3R get one Q free      |
+| S    | 30    |                        |
+| T    | 20    |                        |
+| U    | 40    | 3U get one U free      |
+| V    | 50    | 2V for 90, 3V for 130  |
+| W    | 20    |                        |
+| X    | 90    |                        |
+| Y    | 10    |                        |
+| Z    | 50    |                        |
++------+-------+------------------------+"""
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -33,7 +60,7 @@ def checkout(skus):
     ret += 200 * (n_a // 5)
     ret += 130 * ((n_a % 5) // 3)
     ret += 50 * ((n_a % 5) % 3)
-    
+
     ret += 45 * (n_b // 2)
     ret += 30 * (n_b % 2)
 
@@ -44,4 +71,5 @@ def checkout(skus):
     ret += 20 * (n_f // 3)
     ret += 10 * (n_f % 3)
     return ret
+
 
