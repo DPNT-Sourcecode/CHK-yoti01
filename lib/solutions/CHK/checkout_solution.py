fea81ff93ef6ac911d589673166fcbce2af28a8b
sbx_user1051 <sbx_user1051@169.254.158.117>
2021-10-22 14:16:54 +0000

    Fri Oct 22 14:16:54 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -30,25 +30,10 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    n_a = 0
-    n_b = 0
-    n_c = 0
-    n_d = 0
-    n_e = 0
-    n_f = 0
+    count = [0] * 26
     for sku in skus:
-        if(sku == "A"):
-            n_a += 1
-        elif(sku == "B"):
-            n_b += 1
-        elif(sku == "C"):
-            n_c += 1
-        elif(sku == "D"):
-            n_d += 1
-        elif(sku == "E"):
-            n_e += 1
-        elif(sku == "F"):
-            n_f += 1
+        if(sku >= "A" and sku <= "Z"):
+            count[sku - "A"] += 1
         else:
             return -1
     ret = 0
@@ -71,5 +56,6 @@ def checkout(skus):
     ret += 20 * (n_f // 3)
     ret += 10 * (n_f % 3)
     return ret
+
 
 
